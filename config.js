38bc76a9f426d9dfa71d029441d3266e48e29456
plumcraft <olivier.lebhard@gmail.com>
2017-01-29 21:34:40 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,1 @@
-{ module: 'mmm-facial-recognition', config: { useUSBCam: true, interval: 1, logoutDelay: 120, users: ['plumcraft'] } }
+{ module: 'mmm-facial-recognition', config: { useUSBCam: true, interval: 1, logoutDelay: 120, users: ['plumcraft'] } },
